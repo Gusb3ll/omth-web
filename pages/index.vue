90c462ebfc060bfc915cd81387c0953aeb3fcdeb
Kitpipat Jaritwong <77166960+Gusb3ll@users.noreply.github.com>
2022-02-19 16:16:01 +0700

    ✅ text bluring

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <main>
     <div id="index_container" class="flex h-screen">
       <div class="m-auto">
-        <h1 class="handfont text-white font-thin">
+        <h1 id="f" class="handfont text-white font-thin">
           <b class="lg:text-8xl md:text-7xl text-5xl">Coming soon </b><b class="lg:text-6xl md:text-5xl text-3xl">- Q4 2022</b>
         </h1>
       </div>
@@ -16,13 +16,25 @@ export default {
     this.indexAnimation()
   },
   methods: {
+
     indexAnimation () {
+      const targetsele = document.querySelector('#index_container')
       this.$anime({
-        targets: '#index_container',
+        targets: targetsele,
         opacity: [0, 1],
         duration: 1000,
         delay: 2000,
         easing: 'easeOutQuint'
+      })
+      this.$anime({
+        direction: 'alternate',
+        duration: 750,
+        delay: 2000,
+        loop: 4,
+        easing: 'easeOutQuint',
+        update (anim) {
+          targetsele.style.filter = 'blur(' + 5 * anim.progress / 50 + 'px)'
+        }
       })
     }
   }
